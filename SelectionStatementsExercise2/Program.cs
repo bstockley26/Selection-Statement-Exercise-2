cd6b819de4e5cfb090853765def87ec1e2f1be33
Brooks Stockley <bstockley26@gmail.com>
2024-05-01 12:37:27 -0400

    finished class swtich statements

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Which one of the following classes are your favorite?" +
+                " Math, Science, Gym, Art, or Coding");
+
+            string favClass = Console.ReadLine();
+
+            switch (favClass.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Thats crazy math is way to hard!!!");
+                    break;
+                case "science":
+                    Console.WriteLine("I always thought science was pretty cool too!!!");
+                    break;
+                case "gym":
+                    Console.WriteLine("I think we would get along!!!");
+                    break;
+                case "art":
+                    Console.WriteLine("Feel free to paint something for me then Picasso!!");
+                    break;
+                case "coding":
+                    Console.WriteLine("NERD!!!!!!!!!");
+                    break;
+            }
         }
     }
 }
